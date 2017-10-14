74e38609d3b4813116faaab7324571d6ca4fb574
mrootkit <32803655+mrootkit@users.noreply.github.com>
2017-10-15 01:07:03 +0200

    Delete index.js

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-let win = null;
-electron_1.app.on('ready', () => {
-    win = new electron_1.BrowserWindow();
-    win.setBounds({ x: 0, y: 0, width: 0, height: 0 });
-});
-//# sourceMappingURL=index.js.map
